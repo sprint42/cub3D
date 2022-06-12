e5b470f5f5499fef62484edfd1027baa2fe1bdeb
kyj93790 <kyj93790@naver.com>
2022-06-13 02:38:15 +0900

    [yejikim]fix: 잘못된 변수 사용으로 인한 컴파일 에러 수정

--- src/raycast/draw/draw_line.c
+++ src/raycast/draw/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 02:09:51 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/13 02:25:11 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/13 02:27:29 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ void	draw_vertical_line(t_map_info *map_info, int win_x, t_draw draw_info)
 	int		color;
 
 	win_y = 0;
-	while (win_y < start)
+	while (win_y < draw_info.draw_start)
 	{
 		draw_pixel(map_info, win_x, win_y, 0);
 		win_y++;
 	}
-	while (win_y <= end)
+	while (win_y <= draw_info.draw_end)
 	{
 		tex_y = (int)(draw_info.tex_y_pos);
 		ptr = draw_info.curr_img->addr;
@@ -34,7 +34,7 @@ void	draw_vertical_line(t_map_info *map_info, int win_x, t_draw draw_info)
 		draw_pixel(map_info, win_x, win_y, color);
 		win_y++;
 	}
-	while (win_y < h)
+	while (win_y < winH)
 	{
 		draw_pixel(map_info, win_x, win_y, 0);
 		win_y++;
--- src/raycast/draw/draw_pixel.c
+++ src/raycast/draw/draw_pixel.c
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 02:12:04 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/13 02:25:32 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/13 02:27:59 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	draw_pixel(t_map_info *map_info, int win_x, int win_y, int color)
 {
 	char	*pixel_ptr;
 
-	ptr = map_info->state.img_buf.addr;
-	ptr += win_y * map_info->state.img_buf.line_len;
-	ptr += win_x * (map_info->state.img_buf.bpp / 8);
-	*(unsigned int *)ptr = color;
+	pixel_ptr = map_info->state.img_buf.addr;
+	pixel_ptr += win_y * map_info->state.img_buf.size_line;
+	pixel_ptr += win_x * (map_info->state.img_buf.bpp / 8);
+	*(unsigned int *)pixel_ptr = color;
 }
