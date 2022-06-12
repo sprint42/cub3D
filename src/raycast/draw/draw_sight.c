08d97b4cfa537d6b9276217e3424d26e2419e2e9
kyj93790 <kyj93790@naver.com>
2022-06-13 01:13:54 +0900

    fix: 변수 오타 및 컴파일 에러 수정

--- src/raycast/draw/draw_sight.c
+++ src/raycast/draw/draw_sight.c
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:34:19 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/13 01:10:01 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/13 01:13:07 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 
 void	draw_sight(t_map_info *map_info)
 {
-	int		x;
+	int		i;
 	t_ray	ray;
 
-	x = 0;
+	i = 0;
 	while (i < winW)
 	{
+		ray.camera_pos = -2 * i / (double)winW + 1;
 		init_ray(map_info, &ray);
 		i++;
 	}
--- src/raycast/init/init_ray.c
+++ src/raycast/init/init_ray.c
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:46:30 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/13 01:07:48 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/13 01:12:43 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static void	init_step(t_map_info *map_info, t_ray *ray)
 	else
 	{
 		ray->y_step_dir = 1;
-		ray->neytdist_y = (ray->map_y + 1 - state.pos_y) * ray->diststep_y;
+		ray->nextdist_y = (ray->map_y + 1 - state.pos_y) * ray->diststep_y;
 	}
 }
 
@@ -44,12 +44,11 @@ void	init_ray(t_map_info *map_info, t_ray *ray)
 	t_state state;
 
 	state = map_info->state;
-	ray->camera_pos = -2 * x / (double)winW + 1;
 	ray->raydir_x = state.dir_x + state.plane_x * ray->camera_pos;
 	ray->raydir_y = state.dir_y + state.plane_y * ray->camera_pos;
 	ray->map_x = (int)state.pos_x;
 	ray->map_y = (int)state.pos_y;
-	ray->diststep_x = fabs(1 / raydir_x);
-	ray->diststep_y = fabs(1 / raydir_y);
+	ray->diststep_x = fabs(1 / ray->raydir_x);
+	ray->diststep_y = fabs(1 / ray->raydir_y);
 	init_step(map_info, ray);
 }
