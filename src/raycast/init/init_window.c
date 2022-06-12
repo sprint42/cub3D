96036c6c8b8e830ab269690ee69269d4b3f46d0a
kyj93790 <kyj93790@naver.com>
2022-06-13 02:44:25 +0900

    [yejikim]fix: image buffer 생성 시 check하는 null guard 오류 수정

--- src/raycast/init/init_window.c
+++ src/raycast/init/init_window.c
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:46:23 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/13 00:46:23 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/13 02:43:03 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void	init_window_image(t_map_info *map_info)
 {
 	map_info->state.img_buf.ptr = mlx_new_image(map_info->mlx_ptr, winW, winH);
-	if (map_info->state.img_buf.ptr)
+	if (map_info->state.img_buf.ptr == NULL)
 	{
 		printf("Error\nmessage: image buffer initialize failed\n");
 		exit(EXIT_FAILURE);
@@ -27,7 +27,7 @@ static void	init_window_image(t_map_info *map_info)
 void	init_window(t_map_info *map_info)
 {
 	map_info->win_ptr = mlx_new_window(map_info->mlx_ptr, winW, winH, "cub3D");
-	if (map_info->win_ptr == 0)
+	if (map_info->win_ptr == NULL)
 	{
 		printf("Error\nmessage: window initialize failed\n");
 		exit(EXIT_FAILURE);
