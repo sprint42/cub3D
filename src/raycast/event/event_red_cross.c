477f641e7b06c314a10bd843315f3aa1f9e71ef6
Yejin Kim <yejikim@c8r6s10.42seoul.kr>
2022-06-14 19:44:21 +0900

    [yejikim]feat: ESC, RED_CROSS event 발생 시 프로그램 정상 종료

--- src/raycast/event/event_red_cross.c
+++ src/raycast/event/event_red_cross.c
@@ -6,14 +6,15 @@
 /*   By: yejikim <yejikim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 04:35:54 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/14 13:19:48 by yejikim          ###   ########.fr       */
+/*   Updated: 2022/06/14 19:41:31 by yejikim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "raycast.h"
 
-void	event_red_cross(t_map_info *map_info)
+int	event_red_cross(t_map_info *map_info)
 {
 	free_map(&map_info);
 	exit(EXIT_SUCCESS);
+	return (0);
 }
--- src/raycast/event/play.c
+++ src/raycast/event/play.c
@@ -6,7 +6,7 @@
 /*   By: yejikim <yejikim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:34:16 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/14 13:28:22 by yejikim          ###   ########.fr       */
+/*   Updated: 2022/06/14 19:40:36 by yejikim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ void	play(t_map_info *map_info)
 {
 	draw_sight(map_info);
 	mlx_hook(map_info->win_ptr, EVENT_KEY_PRESS, 0, press_key, map_info);
+	mlx_hook(map_info->win_ptr, EVENT_RED_CROSS, 0, event_red_cross, map_info);
 	mlx_loop(map_info->mlx_ptr);
 }
