595a50841a1b2f256f9e5498efd6f6a1145ef8b5
kyj93790 <kyj93790@naver.com>
2022-06-16 00:55:02 +0900

    [yejikim]fix: #19 다양한 케이스를 일반화할 수 있는 로직으로 단순화, 구현 수정

--- src/raycast/event/event_move.c
+++ src/raycast/event/event_move.c
@@ -3,46 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   event_move.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yejikim <yejikim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 04:15:50 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/14 22:36:52 by yejikim          ###   ########.fr       */
+/*   Updated: 2022/06/16 00:53:17 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycast.h"
 
-static int	check_pos_range(t_map_info *map_info, double dx, double dy)
+static int	check_pos_range(t_map_info *map_info, double diff_x, double diff_y)
 {
-	double	diff_x;
-	double	diff_y;
 	int		map_x;
 	int		map_y;
 
 	map_x = (int)map_info->state.pos_x;
 	map_y = (int)map_info->state.pos_y;
-	diff_x = map_info->state.pos_x - map_x;
-	diff_y = map_info->state.pos_y - map_y;
-	if (diff_x == 0)
+	if (diff_x == 0 && diff_y == 0)
 	{
-		if (dx 
-		if (diff_x == 0 && dx <= 0)
-		map_x--;
-		if (diff_y == 0 && dy <= 0)
-		map_y--;	
+		if (map_info->map[map_y - 1][map_x - 1] != '0' || \
+			map_info->map[map_y - 1][map_x] != '0' || \
+			map_info->map[map_y][map_x - 1] != '0')
+			return (0);
+	}
+	else if (diff_x == 0)
+	{
+		if (map_info->map[map_y][map_x - 1] != '0')
+			return (0);
+	}
+	else if (diff_y == 0)
+	{
+		if (map_info->map[map_y - 1][map_x] != '0')
+			return (0);
 	}
 	if (map_info->map[map_y][map_x] != '0')
 		return (0);
 	return (1);
-	// if (map_info->state.pos_x < 0)
-	// 	map_info->state.pos_x = 0;
-	// if (map_info->state.pos_y < 0)
-	// 	map_info->state.pos_y = 0;
-	// if (map_info->state.pos_x >= map_info->width)
-	// 	map_info->state.pos_x = map_info->width - 1;
-	// if (map_info->state.pos_y >= map_info->height)
-	// 	map_info->state.pos_y = map_info->height - 1;
-	
 }
 
 void	move_to_north(t_map_info *map_info)
@@ -50,20 +46,20 @@ void	move_to_north(t_map_info *map_info)
 	t_state	state;
 	double	dx;
 	double	dy;
-	double	origin_x;
-	double	origin_y;
+	double	diff_x;
+	double	diff_y;
 
 	state = map_info->state;
 	dx = state.dir_x / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
 	dy = state.dir_y / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
-	origin_x = map_info->state.pos_x;
-	origin_y = map_info->state.pos_y;
 	map_info->state.pos_x += dx * SPEED;
 	map_info->state.pos_y += dy * SPEED;
-	if (!check_pos_range(map_info, dx, dy))
+	diff_x = map_info->state.pos_x - (int)map_info->state.pos_x;
+	diff_y = map_info->state.pos_y - (int)map_info->state.pos_y;
+	if (!check_pos_range(map_info, diff_x, diff_y))
 	{
-		map_info->state.pos_x = origin_x;
-		map_info->state.pos_y = origin_y;
+		map_info->state.pos_x -= dx * SPEED;
+		map_info->state.pos_y -= dy * SPEED;
 	}
 }
 
@@ -72,20 +68,20 @@ void	move_to_south(t_map_info *map_info)
 	t_state	state;
 	double	dx;
 	double	dy;
-	double	origin_x;
-	double	origin_y;
+	double	diff_x;
+	double	diff_y;
 
 	state = map_info->state;
 	dx = -state.dir_x / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
 	dy = -state.dir_y / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
-	origin_x = map_info->state.pos_x;
-	origin_y = map_info->state.pos_y;
 	map_info->state.pos_x += dx * SPEED;
 	map_info->state.pos_y += dy * SPEED;
-	if (!check_pos_range(map_info, dx, dy))
+	diff_x = map_info->state.pos_x - (int)map_info->state.pos_x;
+	diff_y = map_info->state.pos_y - (int)map_info->state.pos_y;
+	if (!check_pos_range(map_info, diff_x, diff_y))
 	{
-		map_info->state.pos_x = origin_x;
-		map_info->state.pos_y = origin_y;
+		map_info->state.pos_x -= dx * SPEED;
+		map_info->state.pos_y -= dy * SPEED;
 	}
 }
 
@@ -94,20 +90,20 @@ void	move_to_west(t_map_info *map_info)
 	t_state	state;
 	double	dx;
 	double	dy;
-	double	origin_x;
-	double	origin_y;
+	double	diff_x;
+	double	diff_y;
 
 	state = map_info->state;
 	dx = state.dir_y / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
 	dy = -state.dir_x / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
-	origin_x = map_info->state.pos_x;
-	origin_y = map_info->state.pos_y;
 	map_info->state.pos_x += dx * SPEED;
 	map_info->state.pos_y += dy * SPEED;
-	if (!check_pos_range(map_info, dx, dy))
+	diff_x = map_info->state.pos_x - (int)map_info->state.pos_x;
+	diff_y = map_info->state.pos_y - (int)map_info->state.pos_y;
+	if (!check_pos_range(map_info, diff_x, diff_y))
 	{
-		map_info->state.pos_x = origin_x;
-		map_info->state.pos_y = origin_y;
+		map_info->state.pos_x -= dx * SPEED;
+		map_info->state.pos_y -= dy * SPEED;
 	}
 }
 
@@ -116,20 +112,19 @@ void	move_to_east(t_map_info *map_info)
 	t_state	state;
 	double	dx;
 	double	dy;
-	double	origin_x;
-	double	origin_y;
+	double	diff_x;
+	double	diff_y;
 
 	state = map_info->state;
 	dx = -state.dir_y / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
 	dy = state.dir_x / sqrtf(powf(state.dir_x, 2) + powf(state.dir_y, 2));
-	origin_x = map_info->state.pos_x;
-	origin_y = map_info->state.pos_y;
 	map_info->state.pos_x += dx * SPEED;
 	map_info->state.pos_y += dy * SPEED;
-	if (!check_pos_range(map_info, dx, dy))
+	diff_x = map_info->state.pos_x - (int)map_info->state.pos_x;
+	diff_y = map_info->state.pos_y - (int)map_info->state.pos_y;
+	if (!check_pos_range(map_info, diff_x, diff_y))
 	{
-		map_info->state.pos_x = origin_x;
-		map_info->state.pos_y = origin_y;
+		map_info->state.pos_x -= dx * SPEED;
+		map_info->state.pos_y -= dy * SPEED;
 	}
-	// printf("%d, %d\n", (int)state.pos_y, (int)state.pos_x);
 }
